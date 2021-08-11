74edb3a0a0b6e1d3d982edda3330b601d68c9d20
MetaMuffin <metamuffin@yandex.com>
2021-08-11 10:54:26 +0200

    its slow and has bugs, BUT it works

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,3 @@
-
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -7,13 +6,15 @@ pub struct GnixRoute {
     pub host: String,
     pub backend_port: u16,
     pub backend_host: Option<String>,
+    pub backend_path: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GnixConfig {
     pub listen_http: Option<u16>,
     pub listen_https: Option<u16>,
-    pub routes: Vec<GnixRoute>,
+    pub route: Vec<GnixRoute>,
+    pub fallback_route: GnixRoute,
 }
 
 impl ::std::default::Default for GnixConfig {
@@ -21,24 +22,20 @@ impl ::std::default::Default for GnixConfig {
         Self {
             listen_http: Some(8080),
             listen_https: None,
-            routes: vec![
-                GnixRoute {
-                    host: "host-a.local".to_string(),
-                    path: Some("/test".to_string()),
-                    backend_port: 8081,
-                    backend_host: None,
-                },
+            fallback_route: GnixRoute {
+                backend_port: 8080,
+                backend_host: None,
+                path: None,
+                host: "".to_string(),
+                backend_path: None,
+            },
+            route: vec![
                 GnixRoute {
                     host: "host-a.local".to_string(),
                     path: None,
-                    backend_port: 80,
-                    backend_host: Some("example.com".to_string()),
-                },
-                GnixRoute {
-                    host: "host-b.local".to_string(),
-                    path: None,
                     backend_port: 8081,
                     backend_host: None,
+                    backend_path: None,
                 },
             ],
         }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use crate::helper::fail_request;
 use crate::tunnel::proxy_tunnel;
 use config::GnixConfig;
 use helper::HttpClient;
+use hyper::http::uri::Authority;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{header, Body, Client, Method, Request, Response, Server};
+use hyper::{header, Body, Client, Method, Request, Response, Server, Uri};
 use lazy_static::lazy_static;
 use std::convert::Infallible;
 use std::net::SocketAddr;
@@ -39,7 +40,8 @@ async fn start_http() {
 
     let port = {
         // let config = ;
-        let config = CONFIG.read().unwrap().as_ref().unwrap().clone();
+        let config = CONFIG.read().unwrap();
+        let config = config.as_ref().unwrap().clone();
         config
             .listen_http
             .expect("For now, please specify a http port, even if you dont need it")
@@ -60,27 +62,62 @@ async fn start_http() {
     }
 }
 
-async fn proxy(client: HttpClient, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    let headers = req.headers();
-    let host = match headers.get(header::HOST) {
-        Some(h) => h.to_str().unwrap(),
-        None => return Ok(fail_request(req, "no host header").await),
+async fn proxy(client: HttpClient, mut req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let (host, path) = {
+        let headers = req.headers();
+        let host = match headers.get(header::HOST) {
+            Some(h) => h.to_str().unwrap(),
+            None => return Ok(fail_request(req, "no host header").await),
+        };
+        let host = host.split_once(":").unwrap_or((host, "")).0; // trim the port, if it even exists
+        let path = req.uri().path();
+        (host.to_string(), path.to_string())
     };
-    let config = CONFIG.read().unwrap();
-    let config = config.as_ref().unwrap();
+
+    {
+        let config = CONFIG.read().unwrap();
+        let config = config.as_ref().unwrap();
+        let mut route = &config.fallback_route;
+
+        for r in &config.route {
+            if r.host != host {
+                continue;
+            }
+            if let Some(p) = &r.path {
+                if !path.starts_with(p) {
+                    continue;
+                }
+            }
+            route = r;
+            break;
+        }
+
+        let new_host = route
+            .backend_host
+            .clone()
+            .or_else(|| Some("127.0.0.1".to_string()))
+            .unwrap();
+        let new_port = route.backend_port;
+        let new_authority = format!("{}:{}", new_host, new_port)
+            .parse::<Authority>()
+            .unwrap();
+
+        println!("[{}] {} {}", host, req.method(), path);
+        println!("-> {}:{} {}", new_host, new_port, req.uri().path());
+
+        *req.uri_mut() = Uri::builder()
+            .authority(new_authority)
+            .scheme("http")
+            .path_and_query("/index.html")
+            .build()
+            .unwrap();
+    };
 
     // return Ok(fail_request(req, "request failed on purpose").await);
-
-    let host = host.split_once(":").unwrap_or((host, "")).0; // trim the port, if it even exists
-    let path = req.uri().path();
-
-    println!("[{}] {} {}", host, req.method(), path);
 
     if Method::CONNECT == req.method() {
         proxy_tunnel(client, req).await
     } else {
-        let mut proxy_req = Request::from(req);
-        // *proxy_req.uri_mut().host() = proxy;
-        client.request(proxy_req).await
+        client.request(req).await
     }
 }
